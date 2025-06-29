ad1457790b699914f59aeb7934f997e1cd571598
Pravesh Tiwari <104006609+praveshtiwari150@users.noreply.github.com>
2025-06-29 09:24:07 +0530

    Create 2. Get Unicode Code Point at Index.java

---
+++ java/14-strings/w3resource/2. Get Unicode Code Point at Index.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        String str = "w3resource.com";
+        // write your logic here to print character at index 0 and 10
+        
+        System.out.println("The character at position 0 is " + str.codePointAt(3));
+        System.out.println("The character at position 10 is " + str.codePointAt(4));
+    }
+}
