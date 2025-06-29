070ed8028e74c10ec4798a665b9d8a12ebb8567e
Pravesh Tiwari <104006609+praveshtiwari150@users.noreply.github.com>
2025-06-29 09:28:27 +0530

    Create 4. Count Unicode Points in Range.java

---
+++ java/14-strings/w3resource/4. Count Unicode Points in Range.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        String str = "w3resource.com";
+        // write your logic here to print character at index 0 and 10
+        
+        System.out.println("The codepoint count = " + str.codePointCount(1, 10));
+
+    }
+}
